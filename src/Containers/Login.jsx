c7d2b658c161fd81968359ba2eaf3b956f363d56
jhangikhel <gourav.jhangihel@gmail.com>
2020-06-08 14:25:52 +0530

    Add Library And Webpack

--- src/Containers/Login.jsx
+++ src/Containers/Login.jsx
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
+import { FormControl, TextField, OutlinedInput, InputLabel, InputAdornment, Button } from '@material-ui/core';
+import { Visibility, VisibilityOff } from '@material-ui/icons';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        root: {
+            '& .MuiTextField-root': {
+                margin: theme.spacing(1),
+                width: 200,
+            },
+        },
+    }),
+);
 
-class Login extends Component {
-    render() {
-        return (
-            <div>
-                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            </div>
-        );
-    }
+
+
+
+export default function Login() {
+    const classes = useStyles();
+
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField error id="outlined-basic" label="Outlined" variant="outlined" />
+            {/*  <FormControl className={classes.margin} variant="outlined">
+                <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={values.showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    onChange={handleChange('password')}
+                    endAdornment={
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                            >
+                                {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                    }
+                    labelWidth={70}
+                />
+            </FormControl> */}
+            <Button variant="contained">Default</Button>
+        </form>
+    );
 }
-
-export default Login;
